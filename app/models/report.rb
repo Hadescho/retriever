f26ac88c6201ac105550caa3ef8ea9bb5697c361
Nikolay Kotsev <nikolay.t.kotzev@gmail.com>
2018-07-05 12:22:24 +0300

    Add count scopes for all csp attributes in Report

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,12 @@
+# frozen-string-literal: true
+
+# Class representing the CSP reports from the database
 class Report < ApplicationRecord
+  self.per_page = 50
+  CSP_ATTRIBUTES = column_names - %w[id data created_at updated_at]
+  CSP_ATTRIBUTES.each do |column_name|
+    scope "distinct_#{column_name}", lambda {
+      select("#{column_name}, COUNT(#{column_name})").group(column_name)
+    }
+  end
 end
