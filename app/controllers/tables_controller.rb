d380b973572413c1b97b3c8cfec5ed33e7f630b3
Nikolay Kotsev <nikolay.t.kotzev@gmail.com>
2018-07-05 14:19:56 +0300

    Add TablesController and navbar

---
+++ app/controllers/tables_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# Controller responsible for displaying tables containing statistics of the
+# csp_reporter
+class TablesController < ApplicationController
+  def show; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :table, only: [:show]
+
+  root to: 'tables#show'
 end
